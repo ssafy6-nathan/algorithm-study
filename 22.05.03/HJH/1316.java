7c271e44726a46ece03cac5cb9376137f9c8557c
한지희 <g2hhh2ee@gmail.com>
2022-05-01 17:17:13 +0900

    1316 그룹 단어 체커 - 실패 3

--- 22.05.03/HJH/1316.java
+++ 22.05.03/HJH/1316.java
@@ -6,25 +6,25 @@ public class Main1316 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		boolean[] check;
-		int groups = 0;
+		int cnt = 0;
 
+		boolean[] check = new boolean[26];
 		for (int i = 0; i < N; i++) {
-				check = new boolean[26];
 				String str = br.readLine();
-			for (int j = 1; j < str.length(); j++) {
+				check[str.charAt(0)-'a'] =true;
+			for (int j = 0; j < str.length(); j++) {
 				if(str.charAt(j)!=str.charAt(j-1)) {
-					if(check[j-'a']) {
+					if(check[str.charAt(j)-'a']) {
 						break;
-//						check[j-'a']=1;
 					} else {
-						check[j-'a']=true;
-						groups++;
+						check[str.charAt(j)-'a']=true;
+						cnt++;
+						break;
 					}
 				}
 				
 			}
 		}
-		System.out.println(groups);
+		System.out.println(cnt);
 	}
 }
