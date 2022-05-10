1ea9db671672413514f895943120d8bd0685d585
한지희 <57996351+g2hhh2ee@users.noreply.github.com>
2022-05-10 20:12:10 +0900

    Create b2606_바이러스.java

---
+++ 22.05.10/HJH/b2606_바이러스.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b2606_바이러스 {
+	static boolean[][] map;
+	static int n, e, s;
+	static int cnt = 0;
+	static boolean[] visited;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());	// 정점
+		e = Integer.parseInt(br.readLine());	// 간선
+		s = 1; // 시작
+		map = new boolean[n+1][n+1];
+		visited = new boolean[n+1];
+		for (int i = 1; i <= e; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			map[u][v] = map[v][u] = true;
+		}
+		bfs(s);
+		System.out.println(cnt);
+	}
+	private static void bfs(int s) {
+		Queue<boolean> q = new LinkedList<boolean>();
+		q.add(s);
+		visit[s]=true;
+		while(!q.isEmpty()) {
+			int next_n = q.poll();
+			for(int i=1;i<=n;i++) {
+				if(map[next_n][i]==true && visit[i]==false) {
+					q.add(i);
+					visit[i] = true;
+					cnt++;
+				}
+			}
+		}
+		return cnt;
+	}
+}
