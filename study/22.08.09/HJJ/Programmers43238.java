f28cb666745979a50572b82ee58a8c00aaef19e9
황정준 <hsk2454@naver.com>
2022-08-07 23:18:47 +0900

    황정준 문제풀이 임시 저장

---
+++ study/22.08.09/HJJ/Programmers43238.java
@@ -0,0 +1,18 @@
+package study.nathan_algo_study.week24;
+
+/**
+ * 문제이름 : 입국심사
+ * 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/43238
+ */
+
+public class Programmers43238 {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+
+        return answer;
+    }
+}
+
+/*
+
+*/
