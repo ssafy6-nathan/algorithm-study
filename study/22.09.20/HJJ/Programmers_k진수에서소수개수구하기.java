bd09ed7bc7d1471f807c5ae0f0600f624159b832
황정준 <hsk2454@naver.com>
2022-09-19 21:31:00 +0900

    황정준 알고리즘 문제풀이

---
+++ study/22.09.20/HJJ/Programmers_k진수에서소수개수구하기.java
@@ -0,0 +1,12 @@
+package study.nathan_algo_study.week29;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Programmers_k진수에서소수개수구하기 {
+
+}
+/*
+
+ */
---
+++ study/22.09.20/HJJ/Programmers_양궁대회.java
@@ -0,0 +1,92 @@
+package study.nathan_algo_study.week29;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Programmers_양궁대회 {
+    static int[] point = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    static int maxPoint;
+    static int N;
+    static int[] apeach;
+    static ArrayList<int[]> maxPointList;
+
+    public int[] solution(int n, int[] info) {
+
+        maxPoint = -1;
+        N = n;
+        apeach = info;
+        maxPointList = new ArrayList<>();
+        shot(0, 0, new int[11]);
+
+        if (maxPoint == -1)
+            return new int[]{-1};
+
+        Collections.sort(maxPointList, (o1, o2) -> {
+            for (int i = o1.length - 1; i >= 0; i--) {
+                if (o1[i] != o2[i])
+                    return o2[i] - o1[i];
+            }
+            return 0;
+        });
+
+        return maxPointList.get(0);
+    }
+
+    public void shot(int cnt, int start, int[] ryan) {
+        if (N == cnt) {
+            int ryanPoint = 0;
+            int apeachPoint = 0;
+            for (int i = 0; i < ryan.length; i++) {
+                if (apeach[i] == 0 && ryan[i] == 0)
+                    continue;
+
+                if (apeach[i] >= ryan[i])
+                    apeachPoint += point[i];
+                else
+                    ryanPoint += point[i];
+
+            }
+            if (ryanPoint > apeachPoint) {
+                int diff = ryanPoint - apeachPoint;
+                if (maxPoint < diff) {
+                    maxPoint = ryanPoint - apeachPoint;
+                    maxPointList.clear();
+                    maxPointList.add(ryan.clone());
+                } else if (maxPoint == diff)
+                    maxPointList.add(ryan.clone());
+            }
+
+            return;
+        }
+
+        for (int i = start; i < point.length; i++) {
+            if (apeach[i] < ryan[i])
+                continue;
+
+            ryan[i]++;
+            shot(cnt + 1, i, ryan);
+            ryan[i]--;
+        }
+    }
+
+    public int calPointRyan(int[] target) {
+        int sum = 0;
+        for (int i = 0; i < target.length; i++) {
+            if (target[i] != 0 && apeach[i] < target[i])
+                sum += point[i];
+        }
+        return sum;
+    }
+
+    public int calPointApeach(int[] target) {
+        int sum = 0;
+        for (int i = 0; i < target.length; i++) {
+            if (apeach[i] != 0 && apeach[i] >= target[i])
+                sum += point[i];
+        }
+        return sum;
+    }
+}
+/*
+
+ */
