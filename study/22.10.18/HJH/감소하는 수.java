c3f102009160aa0c10b0d4e8126128662bb8e48f
Ji Hee Han <57996351+g2hhh2ee@users.noreply.github.com>
2022-10-17 20:39:58 +0900

    Create 감소하는 수.java

---
+++ study/22.10.18/HJH/감소하는 수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static long N;
+    public static List<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Long.parseLong(br.readLine());
+
+        if (N <= 10) System.out.println(N);
+        else if (N > 1022) System.out.println("-1");
+        else {
+            for (int i = 1; i <= 1023; i++) {
+                long num = 0;
+                int tmp = i;
+                for (int idx = 9; idx >= 0 ; idx--) {
+                    if (tmp % 2 == 1) {
+                        num = 10 * num + idx;
+                    }
+                    tmp /= 2;
+                }
+                Collections.sort(list);
+                list.add(num);
+            }
+            System.out.println(list.get((int) N));
+        }
+    }
+}
