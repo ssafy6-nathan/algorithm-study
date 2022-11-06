515f0935e23b8a657f3a8c2979c86fd3ac9fe076
person00333 <person00333@gmail.com>
2022-11-07 01:47:09 +0900

    이정훈 문제풀이 업로드

---
+++ study/22.11.08/EJH/13116_30번.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for _ in range(T):
+    A, B = map(int, input().split())
+
+    while A != B:
+        if A > B:
+            A //= 2
+        else:
+            B //= 2
+
+    print(A * 10)
