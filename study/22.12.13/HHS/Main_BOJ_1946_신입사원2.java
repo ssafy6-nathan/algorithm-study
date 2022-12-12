585e1ab9c1d20d6116ded552c013f9ede166f779
HYESUNG <87076126+Hyesung-Han@users.noreply.github.com>
2022-12-12 12:23:28 +0900

    Update Main_BOJ_1946_신입사원2.java
    들여쓰기 수정

--- study/22.12.13/HHS/Main_BOJ_1946_신입사원2.java
+++ study/22.12.13/HHS/Main_BOJ_1946_신입사원2.java
@@ -21,11 +21,11 @@ public class Main_BOJ_1946_신입사원2 {
 			int Min = Integer.MAX_VALUE;
 			PriorityQueue<int[]> que = new PriorityQueue<>(new Comparator<int []>() {
 				 
-	            @Override
-	            public int compare(int []o1, int [] o2) {
-	                return o1[0]-o2[0];
-	            }
-	        });
+	            		@Override
+	            		public int compare(int []o1, int [] o2) {
+	                		return o1[0]-o2[0];
+	            		}
+	        	});
 			
 			for(int n = 0; n < N; n++) {
 				token =  new StringTokenizer(br.readLine());
