c370cd409a4cbf3da30310afe32b0222cd36a885
HYESUNG <87076126+Hyesung-Han@users.noreply.github.com>
2022-12-12 12:21:48 +0900

    Update 프로그래머스_위장.java
    불필요한 코드 제거

--- study/22.12.13/HHS/프로그래머스_위장.java
+++ study/22.12.13/HHS/프로그래머스_위장.java
@@ -2,20 +2,15 @@ import java.util.*;
 
 class Solution {
     static int answer;
-    static HashSet<String> clothesName;
     static HashMap<String, Integer> clothesType;
     
     public int solution(String[][] clothes) {
 
-        clothesName = new HashSet<>();
         clothesType = new HashMap<>();
         
         for(int i = 0; i < clothes.length; i++){
-            String Name = clothes[i][0];
             String Type = clothes[i][1];
             
-            if(clothesName.contains(Name)) continue;
-            clothesName.add(Name);
             clothesType.put(Type, clothesType.getOrDefault(Type, 0) +1);
         }
         answer = 1;
