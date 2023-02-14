a773d213b17171e5014c9d44e9b1e7ff79445c17
JIMIN <qkdwlals613@kangwon.ac.kr>
2023-02-15 03:06:09 +0900

    groupAnagrams

---
+++ study/2023/23.02.14/BJM/LeetCode_49.java
@@ -0,0 +1,40 @@
+package week47;
+
+import java.util.*;
+
+public class LeetCode_49 {
+    public static void main(String[] args) {
+        List<List<String>> list;
+        String[][] strs = {{"eat","tea","tan","ate","nat","bat"}, {""}, {"a"}};
+
+        for (String[] str : strs) {
+            list = groupAnagrams(str);
+            for (List<String> strings : list)
+                System.out.print(strings);
+            System.out.println();
+        }
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        // 문자열의 철자들을 정렬하여 key 값으로 설정한 후, 원본 문자열을 value 리스트에 저장
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] words = str.toCharArray();
+            Arrays.sort(words); // char 배열을 정렬시켜 같은 문자열을 가진 단어들을 같은 key 값에 저장하게 한다.
+            String key = new String(words);
+
+            if (!map.containsKey(key))
+                map.put(key, new ArrayList<>());
+
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
+
+// new Arraylist<>(map.values());
+// 는 되는데
+// List<List<String>> list = new Arraylist<>();
+// list = map.values();
+// 는 왜 안되지?
