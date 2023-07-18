a73e27a2660384e1b7d0a3f3bf2795c811925eb8
jeongjun Hwang <47655983+DeerGum@users.noreply.github.com>
2023-07-18 21:28:30 +0900

    [Week61] 황정준 문제풀이 업로드 (#258)
    * 황정준 문제풀이 업로드
    
    * 조이스틱 코드 일부 수정

---
+++ study/2023/23.07.18/HJJ/Programmers_조이스틱.java
@@ -0,0 +1,52 @@
+package study.nathan_algo_study.week61;
+
+/**
+ * 문제이름 : 조이스틱
+ * 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42860
+ */
+
+public class Programmers_조이스틱 {
+    public int solution(String name) {
+        int[] cost = new int[name.length()];
+        int answer = 0;
+
+        for (int i = 0; i < cost.length; i++)
+            answer += Math.min(name.charAt(i) - 'A', 'Z' + 1 - name.charAt(i)); //알파벳변경 코스트 계산
+
+        int moveCnt = Integer.MAX_VALUE;
+        for (int i = 0; i < name.length(); i++) {
+            int nextIdx = i + 1;   //현재 알파벳 이후 처음 만나는 A가 아닌 알파벳 인덱스
+            while (nextIdx < name.length() && name.charAt(nextIdx) == 'A')
+                nextIdx++;
+
+            //좌측, 우측으로 이동하는 포인터
+            int right = i * 2 + name.length() - nextIdx;
+            int left = i + (name.length() - nextIdx) * 2;
+            moveCnt = Math.min(moveCnt, Math.min(left, right));
+        }
+
+        return answer + moveCnt;    //알파벳변경 코스트 + 이동 코스트
+    }
+}
+
+/*
+A 1
+Y 25
+
+24
+26
+
+26 - 24 = 2
+24 - 26 = -2 + 26 = 24
+
+9 0 1
+
+왼쪽 코스트 2
+오른쪽 코스트 3
+
+XX0000XX
+11000111
+
+01000111 왼:6 오:
+^
+*/
